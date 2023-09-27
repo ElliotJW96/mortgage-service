70004d29ede4e5a7335b97e55ed8be3824b533de
Elliot Wilson <elliotwilson96@gmail.com>
2023-09-27 20:13:00 +0100

    Added small business rule for filtering mortgages

--- src/main/java/com/mortgage/MortgageController.java
+++ src/main/java/com/mortgage/MortgageController.java
@@ -30,11 +30,16 @@ public class MortgageController {
         try {
             ArrayList<Mortgage> mortgages = client.getMortgages(request.getHeaders().get("customerId"));
             // Apply business rules
+            ArrayList<Mortgage> validMortgages = new ArrayList<Mortgage>();
+
             for (Mortgage mortgage : mortgages) {
                 mortgage.generateCurrentPropertyValue();
+                if(mortgage.getStatus().equals("OPEN")){
+                    validMortgages.add(mortgage);
+                }
             }
-            // Return the modified customer object - automatically serialised to JSON
-            return mortgages;
+            // Return the modified list of mortgages - automatically serialised to JSON
+            return validMortgages;
         } catch (Exception e) {
             e.printStackTrace();
             throw new HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error fetching customer data.");
